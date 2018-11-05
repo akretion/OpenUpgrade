164a19cb9d4e76f1a9f6b597dc0afc56d54f709b
mreficent <miquel.raich@eficent.com>
2018-11-05 19:33:27 +0100

    [MIG] account

---
+++ addons/account/migrations/12.0.1.1/post-migration.py
@@ -0,0 +1,162 @@
+# Copyright 2018 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+from psycopg2.extensions import AsIs
+
+
+def map_account_payment_term_line_option(cr):
+    openupgrade.map_values(
+        cr,
+        openupgrade.get_legacy_name('option'),
+        'option',
+        [('fix_day_following_month', 'after_invoice_month'),
+         ('last_day_following_month', 'day_following_month'),
+         ('last_day_current_month', 'day_current_month'),
+         ],
+        table='account_payment_term_line', write='sql')
+
+
+def map_account_tax_type_tax_use(cr):
+    cr.execute(
+        """
+        UPDATE account_tax
+        SET type_tax_use = 'adjustment'
+        WHERE type_tax_use = 'none' AND %s = TRUE
+        """, (AsIs(openupgrade.get_legacy_name('tax_adjustment')), ),
+    )
+    cr.execute(
+        """
+        UPDATE account_tax_template
+        SET type_tax_use = 'adjustment'
+        WHERE type_tax_use = 'none' AND %s = TRUE
+        """, (AsIs(openupgrade.get_legacy_name('tax_adjustment')), ),
+    )
+
+
+def fill_bank_statement_line_account_number(cr):
+    cr.execute(
+        """
+        UPDATE account_bank_statement_line absl
+        SET account_number = rpb.acc_number
+        FROM res_partner_bank rpb
+        WHERE absl.bank_account_id = rpb.id AND rpb.acc_number IS NOT NULL
+        """
+    )
+
+
+def fill_account_chart_template_account_code_prefix(cr):
+    cr.execute(
+        """
+        UPDATE account_chart_template act
+        SET bank_account_code_prefix = rc.bank_account_code_prefix
+        FROM res_company rc
+        WHERE act.%s = rc.id AND act.bank_account_code_prefix IS NULL
+        """, (AsIs(openupgrade.get_legacy_name('company_id')), ),
+    )
+    cr.execute(
+        """
+        UPDATE account_chart_template act
+        SET cash_account_code_prefix = rc.cash_account_code_prefix
+        FROM res_company rc
+        WHERE act.%s = rc.id AND act.cash_account_code_prefix IS NULL
+        """, (AsIs(openupgrade.get_legacy_name('company_id')), ),
+    )
+    # we can't use here new transfer_account_code_prefix field of res_company:
+    cr.execute(
+        """
+        UPDATE account_chart_template act
+        SET transfer_account_code_prefix = aa.code
+        FROM res_company rc
+        LEFT JOIN account_account aa ON rc.transfer_account_id = aa.id
+        WHERE act.%s = rc.id
+        """, (AsIs(openupgrade.get_legacy_name('company_id')), ),
+    )
+
+
+def fill_account_payment_partner_bank_account_id(cr):
+    cr.execute(
+        """
+        UPDATE account_payment ap
+        SET partner_bank_account_id = COALESCE(rp_rpb_rel.rpb, rp_rpb_rel2.rpb)
+        FROM res_partner rp
+        LEFT JOIN (
+            SELECT min(rpb.id) as rpb, rp.id
+            FROM res_partner rp
+            INNER JOIN res_partner_bank rpb ON rpb.partner_id = rp.id
+            GROUP BY rp.id
+            ) rp_rpb_rel ON rp_rpb_rel.id = rp.id
+        LEFT JOIN res_partner rp2 ON rp.commercial_partner_id = rp2.id
+        LEFT JOIN (
+            SELECT min(rpb.id) as rpb, rp.id
+            FROM res_partner rp
+            INNER JOIN res_partner_bank rpb ON rpb.partner_id = rp.id
+            GROUP BY rp.id
+            ) rp_rpb_rel2 ON rp_rpb_rel2.id = rp2.id
+        WHERE rp.id = ap.partner_id 
+        """
+    )
+
+
+def fill_account_journal_alias_id(env):
+    cr = env.cr
+    cr.execute(
+        """
+        SELECT id
+        FROM account_journal
+        WHERE type = 'purchase'
+        """
+    )
+    journal_model = env["account.journal"]
+    journals = journal_model.browse(cr.fetchone())
+    for journal in journals:
+        journal._update_mail_alias({})
+
+
+def fill_account_invoice_line_display_type(cr):
+    cr.execute(
+        """
+        UPDATE account_invoice_line
+        SET display_type = 'line_section'
+        WHERE %s = IS NOT NULL
+        """, (AsIs(openupgrade.get_legacy_name('layout_category_id')), ),
+    )
+
+
+def update_res_company_account_setup_steps_states(cr):
+    cr.execute(
+        """
+        UPDATE res_company
+        SET account_setup_bank_data_state = 'done'
+        WHERE account_setup_bank_data_done = TRUE
+        """
+    )
+    cr.execute(
+        """
+        UPDATE res_company
+        SET account_setup_fy_data_state = 'done'
+        WHERE account_setup_fy_data_done = TRUE
+        """
+    )
+    cr.execute(
+        """
+        UPDATE res_company
+        SET account_setup_coa_state = 'done'
+        WHERE account_setup_coa_done = TRUE
+        """
+    )
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    cr = env.cr
+    map_account_payment_term_line_option(cr)
+    map_account_tax_type_tax_use(cr)
+    fill_bank_statement_line_account_number(cr)
+    fill_account_chart_template_account_code_prefix(cr)
+    fill_account_payment_partner_bank_account_id(cr)
+    fill_account_journal_alias_id(env)
+    fill_account_invoice_line_display_type(cr)
+    update_res_company_account_setup_steps_states(cr)
+    env['account.group']._parent_store_compute()
+    openupgrade.load_data(
+        cr, 'account', 'migrations/12.0.1.1/noupdate_changes.xml')
---
+++ addons/account/migrations/12.0.1.1/pre-migration.py
@@ -0,0 +1,63 @@
+# Copyright 2018 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+column_copies = {
+    'account_payment_term_line': [
+        ('option', None, None),
+    ],
+}
+
+_column_renames = {
+    'account_tax': [
+        ('tax_adjustment', None),
+    ],
+    'account_tax_template': [
+        ('tax_adjustment', None),
+        ('cash_basis_account', 'cash_basis_account_id'),
+    ],
+    'account_chart_template': [
+        ('company_id', None),
+    ],
+    'account_invoice': [
+        ('reference_type', None),
+    ],
+    'account_invoice_line': [
+        ('layout_category_id', None),
+    ],
+}
+
+_table_renames = [
+    ('stock_incoterms', 'account_incoterms')
+]
+
+_model_renames = [
+    ('stock.incoterms', 'account.incoterms')
+]
+
+_field_renames = [
+    ('res.config.settings', 'res_config_settings', 'group_show_price_subtotal',
+     'group_show_line_subtotals_tax_excluded'),
+    ('res.config.settings', 'res_config_settings', 'group_show_price_total',
+     'group_show_line_subtotals_tax_included'),
+    ('res.config.settings', 'res_config_settings', 'sale_show_tax',
+     'show_line_subtotals_tax_selection'),
+]
+
+xmlid_renames = [
+    ('sale.group_show_price_subtotal',
+     'account.group_show_line_subtotals_tax_excluded'),
+    ('sale.group_show_price_total',
+     'account.group_show_line_subtotals_tax_included'),
+]
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    cr = env.cr
+    openupgrade.rename_tables(cr, _table_renames)
+    openupgrade.rename_models(cr, _model_renames)
+    openupgrade.copy_columns(cr, column_copies)
+    openupgrade.rename_columns(cr, _column_renames)
+    openupgrade.rename_xmlids(cr, xmlid_renames)
+    openupgrade.rename_fields(env, _field_renames)
