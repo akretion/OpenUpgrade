e0019fea6f8dd2ac5c42baa020269e643cfd036e
Sébastien BEAU <sebastien.beau@akretion.com>
2019-12-27 21:12:58 +0100

    Improve perf for processing Layout
    Indeed of inserting layout on every sale order and them remove layout
    line for sale order that do not need it. Just add layout on sale order
    that need it.
    This same me more then 10 hours of computation on database with several
    million of line.

--- addons/sale/migrations/12.0.1.1/pre-migration.py
+++ addons/sale/migrations/12.0.1.1/pre-migration.py
@@ -85,22 +85,14 @@ def fill_sale_order_line_sections(cr):
             0, 0, 0, 'line_section', min(sol.create_uid), min(sol.create_date),
             min(sol.write_uid), min(sol.write_date)
         FROM sale_order_line sol
+        WHERE order_id IN (
+            SELECT order_id
+            FROM sale_order_line
+            WHERE layout_category_id IS NOT NULL)
         LEFT JOIN sale_layout_category slc ON slc.id = sol.layout_category_id
         GROUP BY order_id, layout_category_id
         ORDER BY order_id, layout_category_id, sequence
         """
-    )
-    # We remove recently created sale.order.line for sections on sales orders
-    # where there's no sections at all
-    openupgrade.logged_query(
-        cr, """
-        DELETE FROM sale_order_line
-        WHERE layout_category_id IS NULL
-            AND display_type = 'line_section'
-            AND order_id NOT IN (
-                SELECT order_id FROM sale_order_line
-                WHERE layout_category_id IS NOT NULL
-            )""",
     )
 
 
