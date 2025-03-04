e5536e89b6de5dc8402b90a425fcf7f104c38965
Florian Mounier <paradoxxx.zero@gmail.com>
2025-03-04 14:01:11 +0100

    [OU-ADD] analytic

---
+++ openupgrade_scripts/scripts/analytic/18.0.1.1/post-migration.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade, openupgrade_180
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade_180.convert_company_dependent(
+        env,
+        "account.analytic.plan",
+        "default_applicability",
+    )
